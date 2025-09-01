be011ef3702de5b25d476af12206f5f7c89b4bda
HelitonPontes <powerheliton@hotmail.com>
2025-09-01 02:12:59 -0300

    Desafio Concluido com Sucesso

---
+++ Desafio_Fundamentos_DIO/Estacionamento.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Fundamentos_DIO
+{
+    class Estacionamento
+    {
+        private decimal precoInicial = 0;
+        private decimal precoPorHora = 0;
+        private List<string> veiculos = new List<string>();
+
+        public Estacionamento(decimal precoInicial, decimal precoPorHora)
+        {
+            this.precoInicial = precoInicial;
+            this.precoPorHora = precoPorHora;
+        }
+
+        public void AdicionarVeiculo()
+        {
+            // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
+            // *IMPLEMENTE AQUI*
+            
+            Console.WriteLine("Digite a placa do veiculo para estacionar:");
+            //string veiculo= Console.ReadLine();
+            veiculos.Add(Console.ReadLine());
+
+        }
+
+        public void RemoverVeiculo()
+        {
+            Console.WriteLine("Digite a placa do veiculo para remover:");
+            string veiculo = Console.ReadLine();
+
+            // Pedir para o usuário digitar a placa e armazenar na variável placa
+            // *IMPLEMENTE AQUI*
+            string placa = veiculo;
+
+
+            // Verifica se o veículo existe
+            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            {
+                Console.WriteLine("Digite a quantidade de horas que o veiculo permaneceu estacionado:");
+                int h = Convert.ToInt32(Console.ReadLine());
+                // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
+                // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
+                // *IMPLEMENTE AQUI*
+                int horas = h;
+                decimal valorTotal = precoInicial + precoPorHora * horas;
+
+                // TODO: Remover a placa digitada da lista de veículos
+                // *IMPLEMENTE AQUI*
+                veiculos.Remove(placa);
+
+                Console.WriteLine($"O veiculo {placa} foi removido e o preco total foi de: R$ {valorTotal}");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, esse veiculo nao esta estacionado aqui. Confira se digitou a placa corretamente");
+            }
+        }
+
+        public void ListarVeiculos()
+        {
+            // Verifica se há veículos no estacionamento
+            if (veiculos.Any())
+            {
+                Console.WriteLine("Os veiculos estacionados sao:");
+                // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
+                // *IMPLEMENTE AQUI*
+                foreach(string valor in veiculos)
+                {
+                    Console.WriteLine(valor);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nao ha veiculos estacionados.");
+            }
+        }
+    }
+}
---
+++ Desafio_Fundamentos_DIO/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Desafio_Fundamentos_DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            decimal precoInicial = 0;
+            decimal precoPorHora = 0;
+
+            Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
+                              "Digite o preco inicial:");
+            precoInicial = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Agora digite o preco por hora:");
+            precoPorHora = Convert.ToDecimal(Console.ReadLine());
+
+            // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
+            Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
+
+            string opcao = string.Empty;
+            bool exibirMenu = true;
+
+            // Realiza o loop do menu
+            while (exibirMenu)
+            {
+                Console.Clear();
+                Console.WriteLine("Digite a sua opçao:");
+                Console.WriteLine("1 - Cadastrar veiculo");
+                Console.WriteLine("2 - Remover veiculo");
+                Console.WriteLine("3 - Listar veiculos");
+                Console.WriteLine("4 - Encerrar");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        es.AdicionarVeiculo();
+                        break;
+
+                    case "2":
+                        es.RemoverVeiculo();
+                        break;
+
+                    case "3":
+                        es.ListarVeiculos();
+                        break;
+
+                    case "4":
+                        exibirMenu = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opçao invalida");
+                        break;
+                }
+
+                Console.WriteLine("Pressione uma tecla para continuar");
+                Console.ReadLine();
+            }
+
+            Console.WriteLine("O programa se encerrou");
+        }
+    }
+}
